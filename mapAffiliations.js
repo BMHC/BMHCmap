6640a8cd355877bb703422c86498a2d47aee1f69
BMHC <info@brethrenmennoniteheritage.org>
2026-07-07 11:56:33 -0400

    map

--- mapAffiliations.js
+++ mapAffiliations.js
@@ -1,9 +1,6 @@
 function mapAffiliations(){ return [
 	"German Baptist Brethren",
 	"Church of the Brethren",
-	"Mount Carmel",
-	"Eastern Mount Carmel",
-	"Western Mount Carmel",
 	"Brethren Church",
 	"Old German Baptist Brethren",
 	"(Old) Mennonite Church",
